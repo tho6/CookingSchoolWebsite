540212c7e2634c2bd3a4f8c4bfd0e6f72c1678fc
Jim Chan <logbechan@gmail.com>
2020-03-11 17:22:48 +0000

    Update Video.js

--- Public/Video.js
+++
@@ -1,7 +0,0 @@
-function listItem(newItem){
-    const maxBookMark = 5
-    for(const i = 1; i <= maxBookMark; i++){
-        document.getElementByClass('videoList').appendChild(newItem)
-        
-    }
-}
