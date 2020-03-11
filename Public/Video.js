0f03369f73a1941aa52fb1c3c009186c190a9604
Jim Chan <logbechan@gmail.com>
2020-03-12 00:14:55 +0800

    Initial commit

---
+++ Public/Video.js
@@ -0,0 +1,6 @@
+//async function readMemos() { //method "GET" 
+
+//const videoTag = document.querySelector(".videoContainer source")  
+//if(videoTag.src !=="../uploads/skype2282.mp4"){
+  //  videoTag.setAttribute("src","../uploads/skype2282.mp4") 
+//}
