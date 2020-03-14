6b2aed89d54b5f51a9aca149482765c8de2bf4f1
Jim Chan <logbechan@gmail.com>
2020-03-14 12:09:55 +0000

    Update Video.js

--- Public/Video.js
+++ Public/Video.js
@@ -1,11 +1,21 @@
-videoSelect()
-function videoSelect(){
-    const vidPlayer = document.querySelector('source') 
-    const listItems = document.querySelectorAll('#videoList .listItem');
-for(const listItem of listItems){
-    listItem.addEventListener('click', function(event){
-    alert(event.target.id)
-    vidPlayer.setAttribute("src",event.target.id)
-    })
-}
+//readVideo()
+//async function readVideo(){
+  //  const vidPlayer = document.querySelector('source') 
+  //  const listItems = document.querySelectorAll('#videoList .listItem');
+//for(const listItem of listItems){
+  //  listItem.addEventListener('click', async(event)=>{
+  //  await vidPlayer.setAttribute("src",event.currentTarget.id)
+  //  })
+//}
+//}
+    //<video autoplay controls id="video1">
+    //<source src="./skype2282.mp4" type="video/mp4">
+    //Your Browser does not support MP4 videos, please change browser!
+    //</video>  
+  
+async function readVideo(){
+    const fetchRes = await fetch('/videos')
+    const videos = await fetchRes.json()
+    //document.querySelector('.memos').innerHTML = ''; // html file class memos , empty is required, to avoid duplicated memo 
+
 }
