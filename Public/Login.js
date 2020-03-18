45c7481280077fa6745685ab6d6c81ca26059769
“Tavin <chingching6@gmail.comgit config --global user.name Tavin Hogit config --global user.email chingching6@gmail.com>
2020-03-18 11:27:11 +0800

    fixed google

--- Public/Login.js
+++ Public/Login.js
@@ -1,14 +1,14 @@
-window.onload = function() {
-    const searchParams = new URLSearchParams(window.location.search);
-    const errMessage = searchParams.get("error");
+// window.onload = function() {
+//     const searchParams = new URLSearchParams(window.location.search);
+//     const errMessage = searchParams.get("error");
   
-    if (errMessage) {
-      const alertBox = document.createElement("div");
-      alertBox.classList.add("alert", "alert-danger");
-      alertBox.textContent = errMessage;
-      document.querySelector("#error-message").appendChild(alertBox);
-    } else {
-      document.querySelector("#error-message").innerHTML = "";
-    }
-  };
+//     if (errMessage) {
+//       const alertBox = document.createElement("div");
+//       alertBox.classList.add("alert", "alert-danger");
+//       alertBox.textContent = errMessage;
+//       document.querySelector("#error-message").appendChild(alertBox);
+//     } else {
+//       document.querySelector("#error-message").innerHTML = "";
+//     }
+//   };
   
