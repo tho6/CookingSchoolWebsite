2435790eb0e99a4d95ae0be13d24e6cea0afc062
“Tavin <chingching6@gmail.comgit config --global user.name Tavin Hogit config --global user.email chingching6@gmail.com>
2020-03-18 15:50:10 +0800

    updated

--- Public/Video.js
+++ Public/Video.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
    //let myStr = 'Your Browser does not support MP4 videos, please change browser!' 
    let videoHTML = []
    readVideos();
@@ -53,45 +52,3 @@ readVideos()
       })
       readVideos()
   })
-=======
-   //let myStr = 'Your Browser does not support MP4 videos, please change browser!' 
-   let videoHTML = []
-   readVideos();
-async function readVideos(){
-    const fetchRes = await fetch('/videos')
-    const videos = await fetchRes.json()
-    document.querySelector('#videoList').innerHTML = ''; // html file class memos , empty is required, to avoid duplicated memo 
-    
-    for (let i = 0; i < videos.length; i++) {
-      videoHTML = `<li class="listItem">`;
-      videoHTML = videoHTML + videos[i].content + '</li>';
-      if (videos[i].Video != null) {
-      videoHTML = `<li id="${videos[i].Video}" class="listItem">`
-      videoHTML = videoHTML + videos[i].content + '</li>';
-      }
-      document.querySelector('#videoList').innerHTML += videoHTML; // add something into innerHTML      
-    }
-    //const videoPlayerHTML = `<source src="./`
-    const listItems = document.querySelectorAll('#videoList .listItem');
-    for(const listItem of listItems){
-    listItem.addEventListener('click',(event)=>{
-    //document.querySelector('#videoPlayer').innerHtml = `${videoPlayerHTML}${event.target.id}" type="video/mp4">${myStr}`
-    document.querySelector('#videoPlayer').src = `./${event.target.id}`
-})}
-
-}
-    document.querySelector('#addMessageForm').addEventListener('submit', async (event) => {
-      const form = event.currentTarget // currentTarget only available on Form tag
-      event.preventDefault()
-      //if(document.querySelector('#addMessageForm textarea').value == ""){ // adopt innerHtml or Value depends on typing textBox or not 
-
-      //}
-      //if(document.querySelector('#addMessageForm textarea')
-      const formData = new FormData(form)
-      await fetch('/videos',{
-        method: 'POST',
-        body: formData
-      })
-      readVideos()
-  })
->>>>>>> 0459701e51e07146a7525fbe4c9006f4c1b6a7d5
