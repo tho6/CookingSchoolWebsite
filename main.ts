5a074dc6d338ad8d83bb523ddd206cabf75b0b9e
“Tavin <chingching6@gmail.comgit config --global user.name Tavin Hogit config --global user.email chingching6@gmail.com>
2020-03-21 18:05:30 +0800

    updated

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ import {CommentService} from "./services/CommentService"
 import path from 'path'
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(
