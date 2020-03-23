b43029682cc1b82583c665ba29cf4f5b3f64e86a
Ivan Cheung <ivan.cheungickn@gmail.com>
2020-03-23 12:53:22 +0800

    update categories' html

--- Private/Message.js
+++ Private/Message.js
@@ -1,9 +1,14 @@
-let currentPath = "localhost:8080/api/v1/comment/hi/bye"
+// let currentPath = "localhost:8080/api/v1/comment/hi/bye"
+currentPath = window.location.href
+console.log(currentPath)
 currentPath = currentPath.split('.')[0];
 currentPath = currentPath.split('/');
-currentPath = currentPath.splice(currentPath.indexOf('comment') + 1);
-const category = currentPath[0];
-const dish = currentPath[1];
+// currentPath = currentPath.splice(currentPath.indexOf('comment') + 1);
+currentPath = currentPath.splice(-2);
+
+console.log(currentPath);
+const category = decodeURIComponent(currentPath[0])
+const dish = decodeURIComponent(currentPath[1])
 console.log(category, dish);
 let username = '';
 let firstTime = true;
